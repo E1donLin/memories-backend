4bc266d6596a1f6cb43da22d1109fb3bbb4134db
Yujun Lin <yl4039@g.rit.edu>
2021-07-17 16:49:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js'
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+
+const requestLogger = (req, res, next) => {
+  console.log('Method: ', req.method)
+  console.log('Path: ', req.path)
+  console.log('Body: ', req.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(requestLogger)
+
+app.use('/posts', postRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Hello to Memories API')
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
